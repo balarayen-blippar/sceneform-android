a24b7d77603531d3f3aed8078e36ed2ec48824e9
balarayen-blippar <balamurugan.souppourayen@blippar.com>
2023-01-25 17:38:23 +0530

    Disabled fat-aar. Copy individual aars to release folder for now

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 ./build_filament.sh
 ./build_sceneform.sh
+cp ./core/build/outputs/aar/core-release.aar release/
+cp ./ux/build/outputs/aar/ux-release.aar release/
 cp ./sceneform/build/outputs/aar/sceneform-release.aar release/
+cp ./libs/filamat-android/filamat-android-release.aar release/
+cp ./libs/filament-android/filament-android-release.aar release/
+cp ./libs/filament-utils-android/filament-utils-android-release.aar release/
+cp ./libs/gltfio-android/gltfio-android-release.aar release/
